1189a6feec418e9ba6c2eb8178b736d8a741e9e1
ChangshinKang <ckdtls971124@gmail.com>
2022-11-10 00:00:57 +0900

    commit_1110

---
+++ AnonymousHead.java
@@ -0,0 +1,15 @@
+package headMoving;
+
+public class AnonymousHead  extends Head {
+
+	public static void main(String[] args) {
+		AnonymousHead Chulsoo=new AnonymousHead();
+
+		System.out.println(Chulsoo.introdueMyHead);
+		Chulsoo.neckMoving();
+
+		AnonymousHead ChulsooEyes=new AnonymousHead();
+		
+	}
+
+}
